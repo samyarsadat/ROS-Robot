3fc2c50b18d23161b02fa0c36d637733652c4955
Samyar Sadat Akhavi <samyarsadat@gigawhat.net>
2025-01-22 06:12:26 +0300

    Fixed setup script.

--- source_code/ros_ws_robot_infra/deployment/setup_robot.bash
+++ source_code/ros_ws_robot_infra/deployment/setup_robot.bash
@@ -15,7 +15,7 @@ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/vulca
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
